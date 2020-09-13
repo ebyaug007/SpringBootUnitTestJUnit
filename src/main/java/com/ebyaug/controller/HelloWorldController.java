b1cdd39d178f1c43f6d6100aed8024b6a2fc8e88
ebyaug007 <ebyaug007@gmail.com>
2020-09-13 08:07:15 +0530

    Testing controller with JSON return type

--- src/main/java/com/ebyaug/controller/HelloWorldController.java
+++ src/main/java/com/ebyaug/controller/HelloWorldController.java
@@ -3,12 +3,18 @@ package com.ebyaug.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ebyaug.models.Item;
+
 @RestController
 public class HelloWorldController {
 	@GetMapping("/helloworld")
-	public String HelloWorld()
-	{
+	public String HelloWorld() {
 		return "Hello World";
+	}
+
+	@GetMapping("/dummy-item")
+	public Item getItem() {
+		return new Item(1, "Foo", 10, 200);
 	}
 
 }
--- src/test/java/com/ebyaug/ControllerTest.java
+++ src/test/java/com/ebyaug/ControllerTest.java
@@ -1,6 +1,7 @@
 package com.ebyaug;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import java.awt.PageAttributes.MediaType;
 
@@ -26,6 +27,16 @@ class ControllerTest {
 		MvcResult result = mockmvc.perform(request).andReturn();
 		assertEquals("Hello World", result.getResponse().getContentAsString());
 				
+	}
+	
+	@Test
+	void getItemBasic() throws Exception
+	{
+		RequestBuilder request = MockMvcRequestBuilders
+				.get("/dummy-item").accept(org.springframework.http.MediaType.APPLICATION_JSON);
+		MvcResult result = mockmvc.perform(request)
+				.andExpect(content().json("{\"id\": 1,\"name\":\"Foo\",\"price\":10,\"quantity\":200}"))
+				.andReturn();
 	}
 
 }
